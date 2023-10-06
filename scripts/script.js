92e2981342835f5e515f76e0c3b74d8d093a52bd
SerhiiLytvin <slytvyn20@gmail.com>
2023-10-06 23:46:46 +0300

    updated

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+'use strict';
+/* hover over contacts__link  */
+const contactsLinkEnvelope = document.querySelector('.contacts__link-envelope');
+const contactsLinkSmartphone = document.querySelector('.contacts__link-smartphone');
+
+/*--Envelope--*/
+contactsLinkEnvelope.addEventListener('mouseenter', () => {
+   if (contactsLinkEnvelope.classList.contains('contacts__icon-envelope')) {
+      contactsLinkEnvelope.classList.remove('contacts__icon-envelope');
+   }
+   contactsLinkEnvelope.classList.add('contacts__icon-envelope--hover');
+
+});
+
+contactsLinkEnvelope.addEventListener('mouseleave', (e) => {
+   if (contactsLinkEnvelope.classList.contains('contacts__icon-envelope--hover')) {
+      contactsLinkEnvelope.classList.remove('contacts__icon-envelope--hover');
+   }
+   contactsLinkEnvelope.classList.add('contacts__icon-envelope');
+
+});
+
+/*--Smartphone--*/
+contactsLinkSmartphone.addEventListener('mouseenter', () => {
+   if (contactsLinkSmartphone.classList.contains('contacts__icon-smartphone')) {
+      contactsLinkSmartphone.classList.remove('contacts__icon-smartphone');
+   }
+   contactsLinkSmartphone.classList.add('contacts__icon-smartphone--hover');
+
+});
+
+contactsLinkSmartphone.addEventListener('mouseleave', () => {
+   if (contactsLinkSmartphone.classList.contains('contacts__icon-smartphone--hover')) {
+      contactsLinkSmartphone.classList.remove('contacts__icon-smartphone--hover');
+   }
+   contactsLinkSmartphone.classList.add('contacts__icon-smartphone');
+
+});
